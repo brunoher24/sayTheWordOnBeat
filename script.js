73428af543c2bcc9980127839b1eb0a86543cbeb
brunoher24 <brunoher24@gmail.com>
2026-02-13 20:52:26 +0100

    play HTMLAudioElement on page

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ let activeImgIndex = -1;
 // dans un tableau l'index du premier élément est 0
 // console.log(imagesList[1]); // deuxième élément de la liste d'images (son index est 1).
 
-
-function selectNextImgInList() {
+ function selectNextImgInList() {
     if(activeImgIndex > -1) { // si c'est la première fois qu'on clique sur le bouton, il n'y a pas d'image précédente dans la liste
         imagesList[activeImgIndex].className = ""; // on retire la class "active" de l'image précédente 
     }
@@ -21,11 +20,17 @@ function selectNextImgInList() {
     }
     imagesList[activeImgIndex].className = "active"; // on ajoute la class "active" à l'image suivante dans la liste
 }
-
 // nextImgIsActiveBtn.onclick = selectNextImgInList;
 // on rattache un évènement "click" au bouton de la page
 // en d'autres termes, chaque fois que ce bouton est cliqué, le code de la fonction nommée "selectNextImgInList" s'éxécute
+ 
 
 const interval = setInterval(function() {
     selectNextImgInList(); // le code de la fonction selectNextImgInList est éxécuté toutes les 400 millisecondes (0.4secondes) 
 }, 400);
+
+const audioTrack = new Audio('./audio/say_the_word_on_beat.mp3');
+// création d'un élément Audio
+audioTrack.play();
+// utilisation de la méthode "play" pour jouer le son dans la page
+ 
